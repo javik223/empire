26b8539861ce3b4da5c3e16027e3b36bb450db97
javik223 <javik@live.com>
2017-11-13 19:51:20 +0100

    First commit

---
+++ assets/js/app.js
@@ -0,0 +1,58 @@
+$(document).ready(function() {
+  var servicesHeader = $(".services-block-grid__header");
+  var servicesDropdown = $(".services-block-grid-sub");
+
+  servicesHeader.click(function(e) {
+    if (servicesDropdown.hasClass("hide-for-small-only")) {
+      servicesDropdown
+        .removeClass("hide-for-small-only")
+        .hide(3)
+        .slideDown();
+    } else {
+      servicesDropdown.slideUp(300, function() {
+        servicesDropdown.addClass("hide-for-small-only");
+      });
+    }
+  });
+
+  // Nav menu dropdown
+  var navLinks = $(".nav-links");
+  var menuDropdown = navLinks.find(".dropdown");
+
+  menuDropdown.click(function(e) {
+    var dropDownElement = $(this).find("ul");
+
+    if (!$(this).hasClass(".open")) {
+      dropDownElement.stop().slideDown();
+      $(this).addClass(".open");
+    } else {
+      $(this).removeClass(".open");
+      dropDownElement.stop().slideUp();
+    }
+
+    e.preventDefault();
+  });
+  menuDropdown.hover(
+    function() {
+      var dropDownElement = $(this).find("ul");
+      dropDownElement.stop().slideDown();
+    },
+    function() {
+      var dropDownElement = $(this).find("ul");
+      dropDownElement.stop().slideUp();
+    },
+  );
+
+  // Show menu on mobile devices
+  var menuIcon = $("nav .icon-menu");
+  menuIcon.click(function(e) {
+    if (navLinks.hasClass("show")) {
+      navLinks.slideUp(function() {
+        navLinks.removeClass("show");
+      });
+    } else {
+      navLinks.addClass("show").hide();
+      navLinks.stop().slideDown();
+    }
+  });
+});
---
+++ gulpfile.js
@@ -0,0 +1,96 @@
+const gulp = require("gulp");
+const pug = require("gulp-pug2");
+const gutil = require("gulp-util");
+const plumber = require("gulp-plumber");
+const notify = require("gulp-notify");
+const browserSync = require("browser-sync").create();
+var sass = require("gulp-sass");
+const autoprefixer = require("gulp-autoprefixer");
+const sourcemaps = require("gulp-sourcemaps");
+
+// Gulp Taks -----------------------------------------------------
+// Pug Task
+gulp.task("pug", () => {
+  return gulp
+    .src("src/pug/*.pug")
+    .pipe(plumber({ errorHandler: onError }))
+    .on("error", onError)
+    .pipe(pug())
+    .pipe(gulp.dest("."));
+});
+
+// Sass Task: Compile sass files to css and auto-inject into browsers
+gulp.task("sass", () => {
+  return gulp
+    .src("src/sass/*.scss")
+    .pipe(sourcemaps.init())
+    .pipe(plumber({ errorHandler: onError }))
+    .on("error", onError)
+    .pipe(
+      sass({
+        outputStyle: "compressed",
+        includePaths: ["node_modules/foundation-sites/scss"],
+      }),
+    )
+    .pipe(autoprefixer())
+    .pipe(sourcemaps.write("./sass-maps"))
+    .pipe(gulp.dest("assets/css"))
+    .pipe(browserSync.stream());
+});
+
+// Copy fonts
+gulp.task("fonts", () => {
+  return gulp.src("src/fonts/**/*.*").pipe(gulp.dest("assets/fonts"));
+});
+
+// Copy Images
+gulp.task("images", () => {
+  return gulp.src("src/img/**/*.*").pipe(gulp.dest("assets/img"));
+});
+
+// Copy JS
+gulp.task("js", () => {
+  return gulp.src("src/js/**/*.*").pipe(gulp.dest("assets/js"));
+});
+
+// Default Task
+gulp.task("default", ["pug", "sass", "fonts", "images", "js"], function() {
+  // Initialize Browsersync
+  browserSync.init({
+    server: {
+      baseDir: "./",
+    },
+  });
+  // Watch pugfile and transpile
+  gulp.watch("src/pug/**/*.pug", ["pug"]);
+
+  // Watch sass files
+  gulp.watch("src/sass/**/*.scss", ["sass"]);
+
+  // Watch font files
+  gulp.watch("src/fonts/*.*", ["fonts"]);
+
+  // Watch image files
+  gulp.watch("src/img/*.*", ["images"]);
+
+  // Watch js files
+  gulp.watch("src/js/*.js", ["js"]);
+
+  // Watch html files and reload
+  gulp.watch("./**/*.html").on("change", browserSync.reload);
+
+  // Watch JS files and reload
+  gulp.watch("./assets/js/*.js").on("change", browserSync.reload);
+});
+
+// Utility Functions -----------------------------------------------------
+// Task error handler
+let onError = function(error, message) {
+  notify({
+    title: "Error in Build",
+    message: error.message,
+  }).write(error);
+
+  gutil.log(gutil.colors.bgRed(error.message));
+  this.emit("end");
+};
